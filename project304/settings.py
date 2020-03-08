d0eded096de9e8e0b34640f3b196478ae39e7fa2
YongjoonSeo <koreakkrea12@naver.com>
2020-03-08 23:56:17 +0900

    primitive - settings revised

--- project304/settings.py
+++ project304/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#4w2a4duq@f%y$$a&)h+5b1eo%9l!a@=%2pi^*nf3o6ey3pts_'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#4w2a4duq@f%y$$a&)h+5b1eo%9l!a@=%2pi^*nf3o6ey3pts_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
