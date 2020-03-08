f9070d73ce1aa18e86cf39a24c5b0573ee6e18c6
YongjoonSeo <koreakkrea12@naver.com>
2020-03-09 00:07:25 +0900

    allow host

--- project304/settings.py
+++ project304/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#4w2a4duq@f%y$$a&)h+5b1eo%9l!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
