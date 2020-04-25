f9ecde859f7813e616731dc3850604192c586244
YongjoonSeo <koreakkrea12@naver.com>
2020-04-25 18:57:27 +0900

    meal page created

--- basic/views.py
+++ basic/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'basic/home.html')
+
+def meal(request):
+    return render(request, 'basic/meal.html')
